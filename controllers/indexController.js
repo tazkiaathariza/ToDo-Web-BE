8b335216aa4f399cb7b895867ee4d04baa87a91a
Tazkia Athariza Dhivara <tazkiaatharizadhivara@gmail.com>
2022-11-03 08:23:16 +0700

    added index

---
+++ controllers/indexController.js
@@ -0,0 +1,6 @@
+module.exports = {
+  async index(req, res) {
+    return res.json({ message: 'hello there! welcome to todo' });
+   }
+
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const login = require('./controllers/userController')
 const todo = require('./controllers/todoController');
+const open = require('./controllers/indexController');
 const express = require("express");
 const cors = require("cors");
 
@@ -15,7 +16,7 @@ app.use(
 );
 app.use(cookieParser());
 
-
+app.get("/", open.index);
 app.post("/login", login.userLogin);
 app.post("/task/create", todo.createTask);
 app.delete("/task/delete/:todo_id", todo.deleteTask);

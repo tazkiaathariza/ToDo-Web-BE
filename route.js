ed72649138ca573460d76b16eae262f9d9c76e69
Tazkia Athariza Dhivara <tazkiaatharizadhivara@gmail.com>
2022-11-03 09:36:51 +0700

    check the error

--- route.js
+++ route.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 const router = require("express").Router();
-const login = require('./controllers/userController')
-const todo = require('./controllers/todoController');
+// const login = require('./controllers/userController')
+// const todo = require('./controllers/todoController');
 const open = require('./controllers/indexController');
 const cors = require('cors');
 
 
 router.get("/", open.index);
-router.post("/login", login.userLogin);
-router.post("/task/create", todo.createTask);
-router.delete("/task/delete/:todo_id", todo.deleteTask);
+// router.post("/login", login.userLogin);
+// router.post("/task/create", todo.createTask);
+// router.delete("/task/delete/:todo_id", todo.deleteTask);
 
 
 module.exports = router;
